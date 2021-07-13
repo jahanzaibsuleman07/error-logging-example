9be8b7ceb734b4486bff247c798c9c58629955e6
jahanzaibsuleman07 <jahanzaib.suleman@relayr.io>
2021-07-13 16:59:39 +0200

    example

--- src/Example.js
+++ src/Example.js
@@ -1,15 +1,13 @@
-import * as Sentry from "@sentry/react";
+//import * as Sentry from "@sentry/react";
 
-const methodDoesNotExist = {};
 const Example = () => {
   return (
     <>
       <h1>this is example</h1>
-      <button onClick={methodDoesNotExist.a.b}>Break the wzzorld</button>;
     </>
   );
 };
 
-Sentry.withErrorBoundary(Example, { fallback: "an error has occurred" });
+//Sentry.withErrorBoundary(Example, { fallback: "an error has occurred" });
 
 export default Example;
